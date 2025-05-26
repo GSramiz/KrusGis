3a8e3dfb571fd746e339a7e6b29f6b9da5efebe3
GSramiz <ramyzgasanov@mail.ru>
2025-05-26 22:20:46 +0400

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ image = ee.Image("projects/ee-romantik1994/assets/mosaic_Belgorodskaya_2022-05")
 tile_info = ee.data.getMapId({
     "image": image,
     "bands": ["B4", "B3", "B2"],
-    "min": 0,
-    "max": 3000
+    "min": [0, 0, 0],        # ✅ каждый канал отдельно
+    "max": [3000, 3000, 3000]
 })
 
 xyz_url = f"https://earthengine.googleapis.com/v1/projects/ee-romantik1994/maps/{tile_info['mapid']}/tiles/{{z}}/{{x}}/{{y}}"
